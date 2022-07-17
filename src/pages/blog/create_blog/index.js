3e1a8e852ee70f77b0e9e5c3d4a71d5c55f6c833
DavidCabrera9943 <55370822+DavidCabrera9943@users.noreply.github.com>
2022-07-17 00:31:54 -0400

    adsda

--- src/pages/blog/create_blog/index.js
+++ src/pages/blog/create_blog/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import {Paper, TextField, TextareaAutosize} from '@mui/material'
+import {Paper, TextField} from '@mui/material'
+import styled from 'styled-components'
 
 export const CreateBlogPage = ()=>{
     return (
-        <div>
-        <Paper sx="padding:15px;max-width:750px;">
-            <h1>Creacion de Blogs</h1>
-            <TextField fullWidth="true" color="warning" margin="normal" size="normal" label="Titulo" variant="outlined"  name="title" sx=""/>
-            <TextField
-                multiline="true"
-                label="Cuerpo del Blog"
-                fullWidth="true"
-                minRows="10"
-                sx="
-                    margin-top:10px;"
-                />
-        
-        </Paper>
-        </div>
+        <BasePage>
+            <div/>
+            <Paper sx="padding:15px;max-width:750px;
+                
+            ">
+                <h1>Creacion de Blogs</h1>
+                <TextField fullWidth="true" color="warning" margin="normal" size="normal" label="Titulo" variant="outlined"  name="title" sx=""/>
+                <TextField
+                    multiline="true"
+                    label="Cuerpo del Blog"
+                    fullWidth="true"
+                    minRows="10"
+                    sx="
+                        margin-top:10px;"
+                    />
+            
+            </Paper>
+            <div/>
+
+        </BasePage>
     )
 }
 
+const BasePage = styled.div`
+    display: grid;
+    grid-template-column: 10px auto 10px;
+
+`
