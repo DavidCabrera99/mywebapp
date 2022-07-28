85fb2b08d96b03f48f29fe2080440044eab5cef0
DavidCabrera9943 <55370822+DavidCabrera9943@users.noreply.github.com>
2022-07-28 15:00:22 -0400

    Update path.js

--- src/path.js
+++ src/path.js
@@ -1,4 +1,4 @@
-export const PATH = "http://serverapi.onrender.com"
+export const PATH = "https://serverapi.onrender.com"
 
 export const ADS = "<script type=\"text/javascript\" src=\"https://udbaa.com/bnr.php?section=downbar&pub=182822&format=120x600&ga=g\"></script>\n"+
 "<noscript><a href=\"https://yllix.com/publishers/182822\" target=\"_blank\"><img src=\"//ylx-aff.advertica-cdn.com/pub/120x600.png\" style=\"border:none;margin:0;padding:0;vertical-align:baseline;\" alt=\"ylliX - Online Advertising Network\" /></a></noscript>"
